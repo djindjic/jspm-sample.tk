93d24cc4e9424f955c039c17d383f05f72d53226
Aleksandar Djindjic <djindjic@gmail.com>
2014-11-03 17:52:39 +0100

    angular dep

--- app/config.js
+++ app/config.js
@@ -1,13 +1,14 @@
 System.config({
   "paths": {
     "*": "*.js",
-    "github:*": "app/jspm_packages/github/*.js"
+    "github:*": "jspm_packages/github/*.js"
   }
 });
 
 System.config({
   "map": {
     "ionic": "github:driftyco/ionic@^0.9.27",
+    "angular": "github:angular/bower-angular@^1.3.1",
     "github:driftyco/ionic@0.9.27": {
       "css": "github:systemjs/plugin-css@0.1.0",
       "angular": "github:angular/bower-angular@^1.3.1"
