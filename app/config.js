315c0b4d20f2d5ad78b43de1ea17125b1865f2b7
Aleksandar Djindjic <djindjic@gmail.com>
2014-11-18 20:12:01 +0100

    config generated

--- app/config.js
+++ app/config.js
@@ -4,3 +4,35 @@ System.config({
     "github:*": "jspm_packages/github/*.js"
   }
 });
+
+System.config({
+  "map": {
+    "angular": "github:angular/bower-angular@^1.3.3",
+    "ionic": "github:driftyco/ionic-bower@1.0.0-beta.13",
+    "github:angular/bower-angular-animate@1.3.3": {
+      "angular": "github:angular/bower-angular@^1.3.3"
+    },
+    "github:angular/bower-angular-sanitize@1.3.3": {
+      "angular": "github:angular/bower-angular@^1.3.3"
+    },
+    "github:driftyco/ionic-bower@1.0.0-beta.13": {
+      "angular": "github:angular/bower-angular@^1.3.3",
+      "css": "github:systemjs/plugin-css@0.1.0",
+      "angular-ui-router": "github:angular-ui/ui-router@0.2.10",
+      "angular-animate": "github:angular/bower-angular-animate@^1.3.2",
+      "angular-sanitize": "github:angular/bower-angular-sanitize@^1.3.2"
+    }
+  }
+});
+
+System.config({
+  "versions": {
+    "github:angular-ui/ui-router": "0.2.10",
+    "github:angular/bower-angular": "1.3.3",
+    "github:angular/bower-angular-animate": "1.3.3",
+    "github:angular/bower-angular-sanitize": "1.3.3",
+    "github:driftyco/ionic-bower": "1.0.0-beta.13",
+    "github:systemjs/plugin-css": "0.1.0"
+  }
+});
+
