c55ed369d7ce7893c8f84e5c078e0d579395acaa
Aleksandar Djindjic <djindjic@gmail.com>
2015-01-29 18:28:16 +0100

    firefox capabilities

--- sauce_labs_capabilities.js
+++ sauce_labs_capabilities.js
@@ -1,26 +1,26 @@
 module.exports.capabilities = {
-  sl_chrome: {
-    base: 'SauceLabs',
-    browserName: 'chrome',
-    platform: 'Linux',
-    version: '35'
-  },
+  // sl_chrome: {
+  //   base: 'SauceLabs',
+  //   browserName: 'chrome',
+  //   platform: 'Linux',
+  //   version: '35'
+  // },
   sl_firefox: {
     base: 'SauceLabs',
     browserName: 'firefox',
     platform: 'Linux',
     version: '31'
-  },
-  sl_ios_safari: {
-    base: 'SauceLabs',
-    browserName: 'safari',
-    platform: 'OS X 10.8',
-    version: '6'
-  },
-  sl_ie_9: {
-    base: 'SauceLabs',
-    browserName: 'internet explorer',
-    platform: 'Windows 7',
-    version: '9'
-  }
+  }//,
+  // sl_ios_safari: {
+  //   base: 'SauceLabs',
+  //   browserName: 'safari',
+  //   platform: 'OS X 10.8',
+  //   version: '6'
+  // },
+  // sl_ie_9: {
+  //   base: 'SauceLabs',
+  //   browserName: 'internet explorer',
+  //   platform: 'Windows 7',
+  //   version: '9'
+  // }
 };
