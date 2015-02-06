0213817e77bc8c357a2d3afc1cece537720d5ff9
Aleksandar Djindjic <djindjic@gmail.com>
2015-02-06 15:17:47 +0100

    fix package.json

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 var Promise = require('promise'),
     gulp    = require('gulp'),
     $       = require('gulp-load-plugins')();
-    pkg     = require('./package.json');
 
 var startServer = function(){
   return new Promise(function (fulfil) {
