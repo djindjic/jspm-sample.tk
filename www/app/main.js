1f5e77aedf795a65737484f6d9285fbd7ef81aab
Aleksandar Djindjic <djindjic@gmail.com>
2015-02-08 12:37:34 +0100

    Update main.js

--- www/app/main.js
+++ www/app/main.js
@@ -1,6 +1,6 @@
 import angular from 'angular';
 
-export let mainModule = angular.module('jspm-sample', []);
+export let mainModule = angular.module('jspm-sample', ['ngMaterial']);
 
 mainModule.controller('HomeController', ['$scope', function ($scope) {
 	$scope.greetMe = 'world';
