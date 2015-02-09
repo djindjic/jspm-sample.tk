6354c5f201106839ebf8b2eb7c922c11ab577327
Aleksandar Djindjic <djindjic@gmail.com>
2015-02-09 17:24:15 +0100

    all in one bundle

--- www/app/main.js
+++ www/app/main.js
@@ -1,4 +1,5 @@
 import angular from 'angular';
+import 'angular-material';
 
 export let mainModule = angular.module('jspm-sample', ['ngMaterial']);
 
--- www/config.js
+++ www/config.js
@@ -3,19 +3,15 @@ System.config({
     "*": "*.js",
     "github:*": "jspm_packages/github/*.js",
     "npm:*": "jspm_packages/npm/*.js",
-    "jspm-sample/*": "app/*.js"
+    "jspm-sample/*": "app/*.js",
+    "jspm-sample.tk/*": "app/*.js"
   }
 });
-
-// System.config({
-//   traceurOptions: {
-//     annotations: true
-//   }
-// });
 
 System.config({
   "map": {
     "angular": "github:angular/bower-angular@1.3.12",
+    "angular-material": "github:angular/bower-material@0.7.1",
     "angular-mocks": "github:angular/bower-angular-mocks@1.3.12",
     "angular-ui-router": "github:angular-ui/ui-router@0.2.13",
     "css": "github:systemjs/plugin-css@0.1.0",
@@ -23,8 +19,20 @@ System.config({
     "github:angular-ui/ui-router@0.2.13": {
       "angular": "github:angular/bower-angular@1.3.12"
     },
+    "github:angular/bower-angular-animate@1.3.13": {
+      "angular": "github:angular/bower-angular@1.3.12"
+    },
+    "github:angular/bower-angular-aria@1.3.13": {
+      "angular": "github:angular/bower-angular@1.3.12"
+    },
     "github:angular/bower-angular-mocks@1.3.12": {
       "angular": "github:angular/bower-angular@1.3.12"
+    },
+    "github:angular/bower-material@0.7.1": {
+      "angular": "github:angular/bower-angular@1.3.12",
+      "angular-animate": "github:angular/bower-angular-animate@1.3.13",
+      "angular-aria": "github:angular/bower-angular-aria@1.3.13",
+      "css": "github:systemjs/plugin-css@0.1.0"
     }
   }
 });
